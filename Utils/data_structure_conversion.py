214bb26ccf2efe8c6074e287a681ed13dae4468c
AnkushBhise <ankushbhise.18@gmail.com>
2020-09-17 12:00:10 -0400

    Creating Structure for Lib

---
+++ Utils/data_structure_conversion.py
@@ -0,0 +1,15 @@
+import itertools
+
+
+class ListConversion:
+	
+	@staticmethod
+	def list_of_tuple_to_list(list_of_tuple):
+		"""
+
+		:param list_of_tuple: list_of_tuple
+		:type list_of_tuple: list
+		:return: list
+		:rtype: list
+		"""
+		return list(itertools.chain(*list_of_tuple))
