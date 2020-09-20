67ea763e086f96c498fa7bd9f58f61d3d38df784
AnkushBhise <ankushbhise.18@gmail.com>
2020-09-20 06:48:20 -0400

    Version 0.1.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(os.path.join(HERE, "README.md")) as fid:
 # This call to setup() does all the work
 setup(
     name="database_operations",
-    version="1.0.0",
+    version="0.1.0",
     description="Work with Database as it is dataframe, No need to remember sql queries",
     long_description=README,
     long_description_content_type="text/markdown",
